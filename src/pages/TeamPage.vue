21c63c859a007caf83a174401d348fc62da21a54
naorsa198 <63548776+naorsa198@users.noreply.github.com>
2021-06-15 02:22:09 +0300

    checking

--- src/pages/TeamPage.vue
+++ src/pages/TeamPage.vue
@@ -12,7 +12,7 @@
     <template #header>
       <h4 class="mb-0">{{propObj.name}}</h4>
     </template>
-
+<!-- ----------------------------------------------------TeamDetails------------------------------------------- -->
 
     <b-list-group flush>
       <b-list-group-item> {{propObj.continent}} </b-list-group-item>
@@ -21,10 +21,19 @@
       <br>
       <img :src="get_image()" />
       <br>
-      <!-- *********** NEW ******************************** -->
       <b-list-group-item> {{propObj.cuntry}} </b-list-group-item> -->
 
+<!-- ------------------------------------------------TeamPlayers----------------------------------------- -->
+      <div>
+      <span  v-for="res in teamPlayers" :key="res">
+      <router-link to="/PlayerPage" tag="PlayerPreview"  active-class="active" 
+      class="card"  @click.native = PlayerDetail(res) >
+      <PlayerPreview :propObj="res"></PlayerPreview>
+      </router-link>
       
+</span>
+</div>
+
 
       
 
@@ -37,27 +46,37 @@
 </template>
 
 <script>
-    export default {  
+ import  PlayerPreview from "../components/PlayerPreview";
+    export default { 
+      component :{PlayerPreview} ,
       data(){
         return{
           propObj: Object,
           teamid:0,
           resultteam:Object,
           Status:0,
-          result:undefined,
+          results:Object,
       }
       },
     methods: {
+  
+  teamPlayers(){
+    return this.resultteam
+  },
+    PlayerDetail(res){
+    this.$root.store.toPlayerPage(res);
+    },
+
     get_image() {
         if(this.propObj.logo_path != undefined){
         return this.propObj.logo_path;
         }
         else return "'https://cdn.sportmonks.com/images/soccer/placeholder.png"
     },
-    async getTeamFullDetails(){ 
+    async getTeamFullDetails(results){ 
         this.teamid=this.propObj.id;
          try {
-          result = await this.axios.get(
+         results = await this.axios.get(
           `http://localhost:3000/teams/teamFullDetails/${this.teamid}`
           );
       } catch (err) {
@@ -65,14 +84,15 @@
 //if send error so its empty mean = not result for the user
         this.results=[];
       }
-      console.log(result.status);
-      console.log(result);
-      this.resultsteam= result.data
+      console.log(results.status);
+      console.log(results);
+      this.resultsteam= results.data
+      console.log(this.resultsteam)
     }
     },
      
-    mounted() {
-      this.propObj=this.$root.store.teamdetail;
+    beforeMount() {
+    this.propObj=this.$root.store.teamdetail;
     this.getTeamFullDetails(this.result)
       
     },
